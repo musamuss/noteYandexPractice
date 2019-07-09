2f1ddab798d9a97d1bc43e67b8e4e1becdb361b2
musamuss <taller.97@mail.ru>
2019-07-09 19:53:09 +0300

    шаблон набросок окна view

--- noteYandexPractice/Pods/Target Support Files/Pods-noteYandexPractice/Pods-noteYandexPractice-frameworks.sh
+++ noteYandexPractice/Pods/Target Support Files/Pods-noteYandexPractice/Pods-noteYandexPractice-frameworks.sh
@@ -161,13 +161,13 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CorePlot/CorePlot.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CorePlot/CorePlot.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
 fi
 if [[ "$CONFIGURATION" == "Adhoc" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CorePlot/CorePlot.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
